5c5a841bd736bdf0fe24710a3f91888d7ca5bef4
vchrisky <143357101+vchrisky@users.noreply.github.com>
2023-09-27 13:06:40 +0300

    finally completed function game()

--- script.js
+++ script.js
@@ -10,33 +10,38 @@ function getComputerChoice() {
     }
 }
 
-let computerSelection = getComputerChoice();
-let playerSelection = prompt("What is your choice?", "")
-console.log(`Computer chooses ${computerSelection}`);
+
+let computerScore = 0;
+let playerScore = 0;
 
 function playRound(pSelection, cSelection) {
-    choice = pSelection.toLowerCase();
 
-    if (choice == "rock"){
+    if (pSelection == "rock"){
         if (cSelection == "Paper"){
+            computerScore++;
             return "You Lose! Paper covers Rock";
         }else if (cSelection == "Scissors"){
+            playerScore++;
             return "You Win! Rock breaks Scissors";
-        }else if (cSelection == "Rock"){ 
+        }else if (cSelection == "Rock"){
             return "You Draw! Try Again";
         }
-    }else if (choice == "paper"){
+    }else if (pSelection == "paper"){
         if (cSelection == "Rock"){
+            playerScore++;
             return "You Win! Paper covers Rock";
         }else if (cSelection == "Scissors"){
+            computerScore++;
             return "You Lose! Scissors cuts Paper";
         }else if(cSelection == "Paper"){
             return "You Draw! Try Again";
         }
-    }else if (choice == "scissors"){
+    }else if (pSelection == "scissors"){
         if (cSelection == "Rock"){
+            computerScore++;
             return "You Lose! Rock breaks Scissors";
         }else if (cSelection == "Paper"){
+            playerScore++;
             return "You Win! Scissors cuts Paper";
         }else if (cSelection == "Scissors"){
             return "You Draw! Try Again";
@@ -46,5 +51,21 @@ function playRound(pSelection, cSelection) {
     }
 }
 
-console.log(playRound(playerSelection, computerSelection));
 
+function game(){
+    for (let i = 0; (playerScore < 5 && computerScore < 5); i++) {
+        let playerSelection = prompt("Choose! Rock, Paper, or Scissors", "Rock").toLowerCase();
+        let computerSelection = getComputerChoice();
+
+        console.log(`You chose ${playerSelection} and Computer chose ${computerSelection}`);
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(`Player: ${playerScore} Computer: ${computerScore}`);
+        if (playerScore == 5){
+            console.log("**Congratulations! You won Five rounds.**");
+        }else if(computerScore == 5){
+            console.log("**You Lost! Computer won Five rounds.**");
+        }
+    }
+}
+
+console.log(game());
